6cba76b953675fe6d64648ace7a61111b4fe211f
elainees-devs <elainees-devs@gmail.com>
2025-07-07 09:32:06 +0300

    Calculate monthly and projected annual savings with 5% interest based on user input

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+
+projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05) )   # Annual savings projected with 5% interest
+
+print(f"Your monthly savings are: ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
