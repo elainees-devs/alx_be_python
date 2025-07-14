45afa4313958c7211ebce0b68d58b4ea13128ede
elainees-devs <elainees.devs@gmail.com>
2025-07-14 08:45:17 +0300

    Update weather prompt to include suggested input options

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,6 @@
 # This program gives clothing recommendations based on the current weather input by the user.
 
-current_weather = input("What's the weather like today? ")
+current_weather = input("What's the weather like today? (sunny/rainy/cold) ")
 
 if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.\n")
