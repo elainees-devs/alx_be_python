2f002e75ebc0876e8051ea5852e66757fd49eb85
elainees-devs <elainees.devs@gmail.com>
2025-07-14 08:41:22 +0300

    Add weather-based clothing recommendation program

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,12 @@
+# This program gives clothing recommendations based on the current weather input by the user.
+
+current_weather = input("What's the weather like today? ")
+
+if current_weather == "sunny":
+    print("Wear a t-shirt and sunglasses.\n")
+elif current_weather == "rainy":
+    print("Don't forget to carry an umbrella and a raincoat.\n")
+elif current_weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.\n")
+else:
+    print("Sorry, I don't have recommendations for this weather.\n")
