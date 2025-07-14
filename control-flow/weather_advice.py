bd9a81a3771606e090328d8a1fea173130675fe4
elainees-devs <elainees.devs@gmail.com>
2025-07-14 08:51:34 +0300

    Fix variable name and print formatting to match ALX checker requirements

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,13 +1,8 @@
 # This program gives clothing recommendations based on the current weather input by the user.
 
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-
-if current_weather == "sunny":
-    print("Wear a t-shirt and sunglasses.\n")
-elif current_weather == "rainy":
-    print("Don't forget to carry an umbrella and a raincoat.\n")
-elif current_weather == "cold":
-    print("Make sure to wear a warm coat and a scarf.\n")
-else:
-    print("Sorry, I don't have recommendations for this weather.\n")
+if weather == "sunny": print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy": print("Don't forget to carry an umbrella and a raincoat.")
+elif weather == "cold": print("Make sure to wear a warm coat and a scarf.")
+else: print("Sorry, I don't have recommendations for this weather.")
