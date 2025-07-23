4e8a975ee0dfb2c002b5e0a279d3eb46242367b4
elainees-devs <elainees.devs@gmail.com>
2025-07-23 09:31:11 +0300

    Add basic menu structure for shopping list with user choices

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,20 @@
+shopping_list = []
+
+while True:
+    def display_menu():
+        choice = input("Enter your choice: ")
+
+        if choice =='1':
+            # Prompt and add an item
+            pass
+        elif choice == '2':
+            #Prompt and remove an item
+            pass
+        elif choice == '3':
+            #Display the shopping list
+            pass
+        elif choice == '4':
+            print('Goodbye!')
+            break
+        else:
+            print("Invalid choice, please try again later.")
