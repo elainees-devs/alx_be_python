8a641c8c47fcecbe8dbcf753ef4e164f6ba5a1f0
elainees-devs <elainees.devs@gmail.com>
2025-07-23 09:46:55 +0300

    Update promt text

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -12,11 +12,11 @@ while True:
 
         if choice =='1':
             # Prompt and add an item
-           item = input("Enter the item to add")
+           item = input("Enter the item to add: ")
            print(f"'{item}' has been added to your shopping list")
         elif choice == '2':
             #Prompt and remove an item
-            item = input("Enter the item(s) to remove")
+            item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"'{item}' has been removed from your shopping_list:'")
