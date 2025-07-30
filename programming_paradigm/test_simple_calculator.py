e616c59ada5d48027bdb2bb6fdff6010474c0dc6
elainees-devs <elainees.devs@gmail.com>
2025-07-30 10:36:50 +0300

    Replace 'self.calculator' with 'self.calc'

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -10,28 +10,28 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_addition(self):
         """Test the addition method."""
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(0, 0), 0)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtraction(self):
         """Test the subtraction method."""
-        self.assertEqual(self.calculator.subtract(10, 5), 5)
-        self.assertEqual(self.calculator.subtract(-1, -1), 0)
-        self.assertEqual(self.calculator.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
         self.assertEqual(self.calc.subtract(3.5, 1.5), 2.0)
 
     def test_multiplication(self):
         """Test the multiplication method."""
-        self.assertEqual(self.calculator.multiply(4, 5), 20)
-        self.assertEqual(self.calculator.multiply(-2, 3), -6)
-        self.assertEqual(self.calculator.multiply(0, 100), 0)
-        self.assertEqual(self.calculator.multiply(-4, -2), 8)
-        self.assertEqual(self.calculator.multiply(2.5, 2), 5.0)
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-2, 3), -6)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-4, -2), 8)
+        self.assertEqual(self.calc.multiply(2.5, 2), 5.0)
 
     def test_division(self):
         """Test the division method."""
-        self.assertEqual(self.calculator.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(3, 2), 1.5)
         self.assertEqual(self.calc.divide(-6, 3), -2)
         self.assertEqual(self.calc.divide(0, 5), 0)
